8e2c8bff98c8eab0008398ceda90eeb264a1c311
arjunnarang <arjun.12narang@gmail.com>
2026-09-19 13:48:20 +0530

    Creating a bean for open api to add details about the projects like name, version, description

---
+++ src/main/java/com/Arjun/rag/KnowSphere/config/ProjectConfiguration.java
@@ -0,0 +1,30 @@
+package com.Arjun.rag.KnowSphere.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfiguration {
+
+    @Bean
+    public OpenAPI openAPI(){
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("KnowSphere - AI document intelligence. A RAG based backend")
+                                .description("Rest api for KnowSphere where documents of multiple formats can be uploaded for context creation to cater the user queries.")
+                                .version("1.0.0")
+                                .contact( new Contact()
+                                        .name("Arjun Technocrats")
+                                        .email("arjun.12narang@gmail.com")
+                                        .url("abc.com")
+                                )
+
+
+                )
+    }
+}
