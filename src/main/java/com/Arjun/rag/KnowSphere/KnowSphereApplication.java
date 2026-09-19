54c18e2469f6833786f644d97fd711a98a433c9f
arjunnarang <arjun.12narang@gmail.com>
2026-09-19 13:26:13 +0530

    Initial setup of postgres db, rag, vector store, postgres vector db docker

---
+++ src/main/java/com/Arjun/rag/KnowSphere/KnowSphereApplication.java
@@ -0,0 +1,13 @@
+package com.Arjun.rag.KnowSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Arjun/rag/KnowSphere/KnowSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arjun.rag.KnowSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
